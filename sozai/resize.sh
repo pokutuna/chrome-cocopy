8a9dfb4d7c158c7371d6a330ad7531aae8dc44d9
pokutuna <popopopopokutuna@gmail.com>
2019-05-29 06:37:12 +0900

    sozai

--- sozai/resize.sh
+++ sozai/resize.sh
@@ -2,10 +2,11 @@
 
 CWD=$(dirname "$0" && pwd)
 ROOT=$(cd $CWD && cd .. && pwd)
+IN="co2.png"
 OUT=$ROOT/src/img/icon
 
 set -x
 mkdir -p $OUT
 for px in 16 32 48 128; do
-  convert -geometry ${px}x${px} co.png $OUT/${px}.png
+  convert -geometry ${px}x${px} $CWD/$IN $OUT/${px}.png
 done
