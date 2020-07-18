79f2894a6bcadf14c5ab3b89f7c48bbe111ead6b
pokutuna <popopopopokutuna@gmail.com>
2020-07-18 13:32:57 +0900

    mv implementations

--- src/lib/eval.ts
+++ src/lib/eval.ts
@@ -1,18 +1,67 @@
 import {PageTarget, TextTarget, ImageTarget, isTarget} from './target';
 
-export interface EvaluateRequest {
+export interface EvaluatePayload {
   code: string;
   target: PageTarget | TextTarget | ImageTarget;
 }
 
-export interface EvaluateResponse {
-  result: string | null;
+export interface EvaluateResult {
+  result: string | number | null | undefined;
   error?: {
     type: 'ParseError' | 'ExecutionError';
     message: string;
   };
 }
 
-export function isCopyRequest(input: any): input is EvaluateRequest {
+export function isEvaluatePayload(input: any): input is EvaluatePayload {
   return typeof input.code === 'string' && isTarget(input.target);
+}
+
+type AcceptableResult = string | number | null | undefined;
+
+function isAcceptableResult(input: any): input is AcceptableResult {
+  return (
+    typeof input === 'string' ||
+    typeof input === 'number' ||
+    typeof input === 'undefined' ||
+    input === null
+  );
+}
+
+export function evaluate(request: EvaluatePayload): EvaluateResult {
+  let fn: Function;
+  try {
+    fn = eval(request.code);
+    if (typeof fn !== 'function') {
+      throw new Error('evaluating code is not a function');
+    }
+  } catch (e) {
+    return {
+      result: null,
+      error: {
+        type: 'ParseError',
+        message: e.message,
+      },
+    };
+  }
+
+  let result: any;
+  try {
+    result = fn.call(undefined, request.target);
+    if (!isAcceptableResult(result)) {
+      throw new Error(
+        'returning value is not a one of (string | number | null | undefined)'
+      );
+    }
+  } catch (e) {
+    return {
+      result: null,
+      error: {
+        type: 'ExecutionError',
+        message: e.message,
+      },
+    };
+  }
+
+  return {result};
 }
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -5,9 +5,9 @@
  * @packageDocumentation
  */
 
-import {EvaluateResponse, isCopyRequest} from './lib/eval';
+import {isEvaluatePayload, evaluate} from './lib/eval';
 
-const onDOMContentLoaded = new Promise(resolve => {
+const doneDOMContentLoaded = new Promise(resolve => {
   if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => resolve());
   } else {
@@ -17,40 +17,11 @@ const onDOMContentLoaded = new Promise(resolve => {
 
 window.addEventListener('message', (event: MessageEvent) => {
   if (!event.data) return;
-  if (!isCopyRequest(event.data)) return;
+  if (!isEvaluatePayload(event.data)) return;
 
-  const sendResponse = (res: EvaluateResponse) => {
+  doneDOMContentLoaded.then(() => {
+    const result = evaluate(event.data);
     if (!event.source || !event.origin) return;
-    (event.source as Window).postMessage(res, event.origin);
-  };
-
-  let fn: Function;
-  try {
-    fn = eval(event.data.code);
-  } catch (e) {
-    sendResponse({
-      result: null,
-      error: {
-        type: 'ParseError',
-        message: e.message,
-      },
-    });
-    return;
-  }
-
-  onDOMContentLoaded.then(() => {
-    let result: any;
-    try {
-      result = fn.call(undefined, event.data.target);
-    } catch (e) {
-      sendResponse({
-        result: null,
-        error: {
-          type: 'ExecutionError',
-          message: e.message,
-        },
-      });
-    }
-    sendResponse({result});
+    (event.source as Window).postMessage(result, event.origin);
   });
 });
