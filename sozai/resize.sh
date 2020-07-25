32438fb17d9735d06801f3bd75c9656ba89db0e5
pokutuna <mail@pokutuna.com>
2020-07-25 19:18:28 +0900

    アイコンだけ外側ふちどりあり

--- sozai/resize.sh
+++ sozai/resize.sh
@@ -2,11 +2,15 @@
 
 CWD=$(dirname "$0" && pwd)
 ROOT=$(cd $CWD && cd .. && pwd)
-IN="coco.png"
-OUT=$ROOT/src/img/icon
 
 set -x
+
+OUT=$ROOT/src/img/icon
 mkdir -p $OUT
 for px in 16 32 48 128; do
-  convert -geometry ${px}x${px} $CWD/$IN $OUT/${px}.png
+  convert -geometry ${px}x${px} $CWD/"coco.png" $OUT/${px}.png
 done
+
+OUT=$ROOT/src/img
+mkdir -p $OUT
+convert -geometry 360x $CWD/"cocopy.png" $OUT/logo.png
