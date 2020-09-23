0b82c8037aa088caab5a76c4ebdf85912cab1b90
pokutuna <mail@pokutuna.com>
2020-09-23 10:10:28 +0900

    mv

--- src/components/options/Parts.tsx
+++ src/components/options/Parts.tsx
@@ -1,9 +1,8 @@
 import {h, ComponentChildren} from 'preact';
-import {useCallback} from 'preact/hooks';
 import {memo} from 'preact/compat';
 
 import {Link} from 'react-router-dom';
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
 
 export const MainColumn = styled.div`
   padding: 0;
@@ -80,64 +79,6 @@ export const Row = styled.div`
   ${Item} + ${Item} {
     margin-left: ${props => props.theme.space[4]};
   }
-`;
-
-type ButtonMode = 'default' | 'danger';
-
-const primary = css`
-  &:hover,
-  &:focus {
-    color: ${props => props.theme.color.primary};
-    border-color: ${props => props.theme.color.primary};
-    outline-color: ${p => p.theme.color.primary};
-  }
-`;
-
-const danger = css`
-  color: ${p => p.theme.color.danger};
-  &:hover,
-  &:focus {
-    outline-color: ${p => p.theme.color.danger};
-  }
-`;
-
-const ButtonStyle = styled.button<{mode?: ButtonMode}>`
-  padding: ${props => props.theme.space[2]};
-  background-color: transparent;
-  border: solid 1px;
-  border-radius: ${props => props.theme.space[1]};
-  cursor: pointer;
-  ${p => (p.mode === 'danger' ? danger : primary)};
-`;
-
-export const Button = (props: {
-  mode?: ButtonMode;
-  disabled?: boolean;
-  onClick?: (event: MouseEvent) => void;
-  children?: ComponentChildren;
-}) => {
-  const onClick = useCallback(
-    (event: MouseEvent) => {
-      event.preventDefault();
-      props.onClick?.(event);
-    },
-    [props.onClick]
-  );
-
-  return (
-    <ButtonStyle
-      type="button"
-      mode={props.mode}
-      onClick={onClick as any}
-      disabled={props.disabled}
-    >
-      {props.children}
-    </ButtonStyle>
-  );
-};
-
-export const ButtonIcon = styled.i`
-  margin-right: ${p => p.theme.space[1]};
 `;
 
 export const DividerH = styled.div`
