59a817dfdf427a96159925072f9201f4044892af
pokutuna <mail@pokutuna.com>
2020-10-01 04:18:45 +0900

    next

---
+++ gallery/next.config.js
@@ -0,0 +1,5 @@
+const prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: prod ? '/chrome-cocopy/' : '',
+};
---
+++ gallery/pages/index.tsx
@@ -0,0 +1,7 @@
+import {NextPage} from 'next';
+
+const Index: NextPage = () => {
+  return <h1>Hello!</h1>;
+};
+
+export default Index;
