801a7735a6f1c951d67d48b187bbe7f41a9af769
pokutuna <mail@pokutuna.com>
2020-10-05 11:25:40 +0900

    test: improve snapshot tests to wait rendered & mock

--- src/components/options/App.test.tsx
+++ src/components/options/App.test.tsx
@@ -1,8 +1,25 @@
 import {h} from 'preact';
-import render from 'preact-render-to-string';
 
+import {render, screen, waitFor} from '@testing-library/preact';
+import '@testing-library/jest-dom';
+import {chrome} from 'jest-chrome';
+
+import {defaultFunctions} from '../../lib/builtin';
 import {App} from './App';
 
-test('render options', () => {
-  expect(render(<App />, {}, {pretty: true})).toMatchSnapshot();
+test('render options', async () => {
+  chrome.tabs.query.mockImplementation((_, cb) =>
+    cb([{url: 'https://example.test/page'}] as chrome.tabs.Tab[])
+  );
+  chrome.storage.sync.get.mockImplementation((_, cb) =>
+    cb({functions: defaultFunctions})
+  );
+
+  render(<App />);
+
+  await waitFor(() =>
+    expect(screen.getByText(defaultFunctions[0].name)).toBeInTheDocument()
+  );
+
+  expect(document.body).toMatchSnapshot();
 });
